dc92e1cce6fac2089b0bc748b457d9ab124778f5
rampungcak <162382726+rampungcak@users.noreply.github.com>
2025-02-19 11:44:48 +0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
             "name":"admin",
             "url":"https://arielbokep.pages.dev"
             },
-            "foundingDate":"2024-10-17"
+            "foundingDate":"2025-02-18"
         }
         const jsonLd2 = {
             "@context": "https://schema.org",
